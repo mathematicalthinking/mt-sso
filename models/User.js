cba04837cf66c8d5d36ae3624ad1f95d2af407d8
Daniel Kelly <djk8552@gmail.com>
2019-06-05 19:16:39 -0400

    User model

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+/**
+ * @public
+ * @class User
+ * @class User
+ * @description A user is created by signup using passport and authorized by admin
+ * @todo We need to decide how to handle different user types/roles
+ */
+const UserSchema = new Schema(
+  {
+    username: { type: String, trim: true },
+    password: { type: String },
+    mtToken: { type: String },
+    mtTokenExpiryDate: { type: Date },
+    encUserId: { type: ObjectId },
+    vmtUserId: { type: ObjectId },
+    email: { type: String },
+    isTrashed: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('User', UserSchema);
