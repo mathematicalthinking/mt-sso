57e9f121fc25fac8acb1d8d973a288885feb1458
Daniel Kelly <djk8552@gmail.com>
2019-06-07 12:49:10 -0400

    Add allowedDomains

---
+++ constants/allowed_domains.js
@@ -0,0 +1,8 @@
+module.exports = {
+  [process.env.ENC_URL_DEV]: 'EnCOMPASS',
+  [process.env.ENC_URL_STAGING]: 'EnCOMPASS',
+  [process.env.ENC_URL_PROD]: 'EnCOMPASS',
+  [process.env.VMT_URL_DEV]: 'VMT',
+  [process.env.VMT_URL_STAGING]: 'VMT',
+  [process.env.VMT_URL_PROD]: 'VMT',
+};
