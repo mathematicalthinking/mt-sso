5451f831740f3f2e0791bfd0f15b6b92a29cef3f
Daniel Kelly <djk8552@gmail.com>
2019-06-10 09:50:21 -0400

    Use different view for home page depending on logged in/out

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
+const { getUser } = require('../middleware/user-auth');
+const { getEncUrl, getVmtUrl } = require('../middleware/app_urls');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.cookie('redirectURL', req.query.redirectURL);
-  res.render('index', {
+
+  let user = getUser(req);
+  let vmtUrl = getVmtUrl();
+  let encUrl = getEncUrl();
+
+  if (!user) {
+    return res.render('index', {
+      title: 'Mathematical Thinking',
+    });
+  }
+
+  let displayName = user.firstName ? user.firstName : user.username;
+
+  return res.render('home', {
     title: 'Mathematical Thinking',
+    user,
+    encUrl,
+    vmtUrl,
+    displayName,
   });
 });
 
