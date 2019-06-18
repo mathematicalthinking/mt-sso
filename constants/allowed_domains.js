2fce79161cf8b75b10b56f648b961bbe32213e25
Daniel Kelly <djk8552@gmail.com>
2019-06-18 14:15:56 -0400

    Update allowed domains for enc test url

--- constants/allowed_domains.js
+++ constants/allowed_domains.js
@@ -2,6 +2,7 @@ module.exports = {
   [process.env.ENC_URL_DEV]: 'EnCOMPASS',
   [process.env.ENC_URL_STAGING]: 'EnCOMPASS',
   [process.env.ENC_URL_PROD]: 'EnCOMPASS',
+  [process.env.ENC_URL_TEST]: 'EnCOMPASS',
   [process.env.VMT_URL_DEV]: 'VMT',
   [process.env.VMT_URL_STAGING]: 'VMT',
   [process.env.VMT_URL_PROD]: 'VMT',
