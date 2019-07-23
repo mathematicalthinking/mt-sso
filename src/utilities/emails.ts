203aae4b5831635277ea1cdee639f2d34c42da03
Daniel Kelly <djk8552@gmail.com>
2019-07-23 16:07:27 -0400

    Function for sending emails to all admins

--- src/utilities/emails.ts
+++ src/utilities/emails.ts
@@ -2,6 +2,8 @@ import * as nodemailer from 'nodemailer';
 import { getEmailAuth } from '../config/emails';
 import * as emailTemplates from '../constants/email_templates';
 import { EmailTemplateHash, UserDocument } from '../types';
+import { AppNames } from '../config/app_urls';
+import User from '../models/User';
 
 export const sendEmailSMTP = function(
   recipient: string,
@@ -51,4 +53,44 @@ export const sendEmailSMTP = function(
       );
     },
   );
+};
+
+export const sendEmailsToAdmins = async function(
+  host: string,
+  appName: AppNames.Enc | AppNames.Vmt,
+  template: string,
+  relatedUser: UserDocument,
+): Promise<void> {
+  try {
+    let adminCrit =
+      appName === AppNames.Enc
+        ? {
+            isTrashed: false,
+            accountType: 'A',
+            email: { $ne: null },
+          }
+        : {
+            isTrashed: false,
+            isAdmin: true,
+            email: { $ne: null },
+          };
+    let admins: UserDocument[] = await User.find(adminCrit)
+      .lean()
+      .exec();
+
+    if (!Array.isArray(admins)) {
+      return;
+    }
+
+    // relatedUser is who the email is about, i.e. if a new user signed up
+    admins.forEach(
+      (user): void => {
+        if (user.email) {
+          sendEmailSMTP(user.email, host, template, null, relatedUser, appName);
+        }
+      },
+    );
+  } catch (err) {
+    console.error(`Error sendEmailsToAdmins: ${err}`);
+  }
 };
