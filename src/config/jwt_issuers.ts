ab35a5a8f7102d8dde1c2a61de95d274417562af
Daniel Kelly <djk8552@gmail.com>
2019-07-24 16:59:43 -0400

    Use AppNames enum for getIssuerName

--- src/config/jwt_issuers.ts
+++ src/config/jwt_issuers.ts
@@ -1,10 +1,12 @@
+import { AppNames } from '../config/app_urls';
+
 const issuers = {
   [process.env.ENC_JWT_ISSUER_ID]: {
-    name: 'EnCoMPASS',
+    name: AppNames.Enc,
     url: process.env.ENC_URL,
   },
   [process.env.VMT_JWT_ISSUER_ID]: {
-    name: 'Virtual Math Teams',
+    name: AppNames.Vmt,
     url: process.env.VMT_URL,
   },
   [process.env.JWT_ISSUER_ID]: {
