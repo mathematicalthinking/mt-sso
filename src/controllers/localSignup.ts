05de1602bd2fbec71b8916c45ef800dfaf0fb2c6
Daniel Kelly <djk8552@gmail.com>
2019-08-05 11:38:42 -0400

    Use Enc/Vmt models for signup
    Revert any successfully account creations if any 1 of the 3 fail

--- src/controllers/localSignup.ts
+++ src/controllers/localSignup.ts
@@ -1,10 +1,10 @@
 import * as express from 'express';
-import axios, { AxiosResponse } from 'axios';
+import createError from 'http-errors';
+
 import User from '../models/User';
 
 import { getUser } from '../middleware/user-auth';
 import {
-  generateAPIToken,
   setSsoCookie,
   setSsoRefreshCookie,
   generateAccessToken,
@@ -23,106 +23,102 @@ import { sendEmailSMTP, sendEmailsToAdmins } from '../utilities/emails';
 import { CONFIRM_EMAIL_TOKEN_EXPIRY } from '../config/emails';
 import { getResetToken } from '../utilities/tokens';
 import { AppNames } from '../config/app_urls';
+import EncUser from '../models/EncUser';
+import VmtUser from '../models/VmtUser';
 
 export const createEncUser = async (
   mtUser: any,
   requestBody: any,
-  apiToken: string,
   requestBodyType: 'enc' | 'vmt' | 'google',
-): Promise<EncUserDocument> => {
-  let {
-    firstName,
-    lastName,
-    username,
-    email,
-    _id,
-    createdAt,
-    updatedAt,
-    isEmailConfirmed,
-  } = mtUser;
+): Promise<EncUserDocument | null> => {
+  try {
+    let {
+      firstName,
+      lastName,
+      username,
+      email,
+      _id,
+      isEmailConfirmed,
+    } = mtUser;
 
-  let {
-    organization,
-    organizationRequest,
-    location,
-    isAuthorized,
-    requestReason,
-    createdBy,
-    authorizedBy,
-  } = requestBody;
+    let {
+      organization,
+      organizationRequest,
+      location,
+      isAuthorized,
+      requestReason,
+      createdBy,
+      authorizedBy,
+    } = requestBody;
 
-  let accountType =
-    requestBodyType === 'enc' ? requestBody.accountType : EncAccountType.T;
+    let accountType =
+      requestBodyType === 'enc' ? requestBody.accountType : EncAccountType.T;
 
-  let encUserBody = {
-    firstName,
-    lastName,
-    username,
-    email,
-    ssoId: _id,
-    organization,
-    organizationRequest,
-    location,
-    isAuthorized,
-    requestReason,
-    accountType,
-    createdBy,
-    authorizedBy,
-    isEmailConfirmed,
-    actingRole: accountType === 'S' ? 'student' : 'teacher',
-    createDate: createdAt,
-    lastModifiedDate: updatedAt,
-  };
+    let encUserBody = {
+      firstName,
+      lastName,
+      username,
+      email,
+      ssoId: _id,
+      organization,
+      organizationRequest,
+      location,
+      isAuthorized,
+      requestReason,
+      accountType,
+      createdBy,
+      authorizedBy,
+      isEmailConfirmed,
+      actingRole: accountType === 'S' ? 'student' : 'teacher',
+      createDate: new Date(),
+      lastModifiedDate: new Date(),
+    };
 
-  // Should we use create date of SSO account? or Date.now()? ...
-
-  let encApiUrl = `${process.env.ENC_URL}/auth/newMtUser`;
-
-  let config = {
-    headers: { Authorization: 'Bearer ' + apiToken },
-  };
-
-  return axios.post(encApiUrl, encUserBody, config).then(
-    (results: AxiosResponse<any>): EncUserDocument => {
-      return results.data;
-    },
-  );
+    let encUser = await EncUser.create(encUserBody);
+    return encUser;
+  } catch (err) {
+    // something went wrong creating the encompass user
+    return null;
+  }
 };
 
-export const createVmtUser = (
+export const createVmtUser = async (
   mtUser: any,
   requestBody: any,
-  apiToken: string,
   requestBodyType: 'enc' | 'vmt' | 'google',
-): Promise<VmtUserDocument> => {
-  let { firstName, lastName, username, email, _id, isEmailConfirmed } = mtUser;
-  let accountType =
-    requestBodyType === 'vmt'
-      ? requestBody.accountType
-      : VmtAccountType.facilitator;
+): Promise<VmtUserDocument | null> => {
+  try {
+    let {
+      firstName,
+      lastName,
+      username,
+      email,
+      _id,
+      isEmailConfirmed,
+    } = mtUser;
+    let accountType =
+      requestBodyType === 'vmt'
+        ? requestBody.accountType
+        : VmtAccountType.facilitator;
 
-  let vmtUserBody = {
-    firstName,
-    lastName,
-    username,
-    email,
-    ssoId: _id,
-    accountType,
-    isEmailConfirmed,
-    rooms: requestBody.rooms,
-    _id: requestBody._id,
-  };
+    let vmtUserBody = {
+      firstName,
+      lastName,
+      username,
+      email,
+      ssoId: _id,
+      accountType,
+      isEmailConfirmed,
+      rooms: requestBody.rooms,
+      _id: requestBody._id,
+    };
 
-  let vmtApiUrl = `${process.env.VMT_URL}/auth/newMtUser`;
-  let config = {
-    headers: { Authorization: 'Bearer ' + apiToken },
-  };
-
-  return axios.post(vmtApiUrl, vmtUserBody, config).then(
-    (results: AxiosResponse<any>): VmtUserDocument => {
-      return results.data;
-    },
-  );
+    let vmtUser = await VmtUser.create(vmtUserBody);
+    return vmtUser;
+  } catch (err) {
+    console.log('err createVmtUser: ', err);
+    return null;
+  }
 };
 
 export const createBaseUser = async (
@@ -159,86 +155,128 @@ export const encSignup = async (
   res: express.Response,
   next: express.NextFunction,
 ): Promise<void> => {
-  let currentUser = getUser(req);
+  let mtUser;
+  let didCreateMtUser = false;
+  let encUser;
+  let vmtUser;
+  try {
+    let currentUser = getUser(req);
 
-  // Do not want to login a user who is being created by a currently logged in user
-  // Currently only comes from encompass
+    // Do not want to login a user who is being created by a currently logged in user
+    // Currently only comes from encompass
 
-  let doLoginNewUser = currentUser === undefined;
+    let doLoginNewUser = currentUser === undefined;
 
-  let mtUser = req.mt.auth.signup.user;
-  if (mtUser === undefined) {
-    // should never happen
-    return next();
-  }
-  let apiToken = await generateAPIToken(mtUser._id);
+    mtUser = req.mt.auth.signup.user;
+    if (mtUser === undefined) {
+      // should never happen
+      return next();
+    }
 
-  let [encUser, vmtUser] = await Promise.all([
-    createEncUser(mtUser, req.body, apiToken, 'enc'),
-    createVmtUser(mtUser, req.body, apiToken, 'enc'),
-  ]);
-
-  mtUser.encUserId = encUser._id;
-  mtUser.vmtUserId = vmtUser._id;
-
-  await mtUser.save();
-
-  let accessToken;
-  let refreshToken;
-
-  if (doLoginNewUser) {
-    [accessToken, refreshToken] = await Promise.all([
-      generateAccessToken(mtUser),
-      generateRefreshToken(mtUser),
+    [encUser, vmtUser] = await Promise.all([
+      createEncUser(mtUser, req.body, 'enc'),
+      createVmtUser(mtUser, req.body, 'enc'),
     ]);
 
-    setSsoCookie(res, accessToken);
-    setSsoRefreshCookie(res, refreshToken);
-  }
+    if (encUser === null || vmtUser === null) {
+      if (encUser !== null) {
+        // vmt errored but enc was successful
+        // revert creating of enc user
+        await EncUser.findByIdAndDelete(encUser._id);
+      } else if (vmtUser !== null) {
+        await VmtUser.findByIdAndDelete(vmtUser._id);
+      }
+      // both creation processes failed
+      // just return error
+      return next(
+        new createError[500](
+          'Sorry, an unexpected error occured. Please try again.',
+        ),
+      );
+    }
 
-  let userEmail = mtUser.email;
-  if (typeof userEmail === 'string') {
-    let token = await getResetToken(20);
-
-    mtUser.confirmEmailToken = token;
-    mtUser.confirmEmailExpires = Date.now() + CONFIRM_EMAIL_TOKEN_EXPIRY; // 1 day
+    mtUser.encUserId = encUser._id;
+    mtUser.vmtUserId = vmtUser._id;
 
     await mtUser.save();
+    didCreateMtUser = true;
 
-    sendEmailSMTP(
-      userEmail,
-      process.env.ENC_URL,
-      'confirmEmailAddress',
-      token,
-      mtUser,
-      AppNames.Enc,
-    );
+    let accessToken;
+    let refreshToken;
 
-    if (process.env.NODE_ENV === 'production') {
-      sendEmailsToAdmins(
-        process.env.ENC_URL,
-        AppNames.Enc,
-        'newUserNotification',
-        mtUser,
-      );
-    } else {
+    if (doLoginNewUser) {
+      [accessToken, refreshToken] = await Promise.all([
+        generateAccessToken(mtUser),
+        generateRefreshToken(mtUser),
+      ]);
+
+      setSsoCookie(res, accessToken);
+      setSsoRefreshCookie(res, refreshToken);
+    }
+
+    let userEmail = mtUser.email;
+    if (typeof userEmail === 'string') {
+      let token = await getResetToken(20);
+
+      mtUser.confirmEmailToken = token;
+      mtUser.confirmEmailExpires = new Date(
+        Date.now() + CONFIRM_EMAIL_TOKEN_EXPIRY,
+      ); // 1 day
+
+      await mtUser.save();
+
       sendEmailSMTP(
-        process.env.ENC_GMAIL_USERNAME,
+        userEmail,
         process.env.ENC_URL,
-        'newUserNotification',
-        null,
+        'confirmEmailAddress',
+        token,
         mtUser,
         AppNames.Enc,
+      );
+
+      if (process.env.NODE_ENV === 'production') {
+        sendEmailsToAdmins(
+          process.env.ENC_URL,
+          AppNames.Enc,
+          'newUserNotification',
+          mtUser,
+        );
+      } else {
+        sendEmailSMTP(
+          process.env.ENC_GMAIL_USERNAME,
+          process.env.ENC_URL,
+          'newUserNotification',
+          null,
+          mtUser,
+          AppNames.Enc,
+        );
+      }
+    }
+
+    let results = {
+      accessToken,
+      refreshToken,
+      encUser,
+    };
+    res.json(results);
+  } catch (err) {
+    console.log('Error enc signup: ', err);
+    if (didCreateMtUser === false) {
+      // error creating sso user
+      // revert enc / vmt creation if necessary
+      if (encUser) {
+        EncUser.findByIdAndDelete(encUser._id);
+      }
+      if (vmtUser) {
+        VmtUser.findByIdAndDelete(vmtUser._id);
+      }
+      return next(
+        new createError[500](
+          'Sorry, an unexpected error occured. Please try again.',
+        ),
       );
     }
   }
-
-  let results = {
-    accessToken,
-    refreshToken,
-    encUser,
-  };
-  res.json(results);
 };
 
 export const vmtSignup = async (
@@ -246,87 +284,129 @@ export const vmtSignup = async (
   res: express.Response,
   next: express.NextFunction,
 ): Promise<void> => {
-  let currentUser = getUser(req);
+  let mtUser;
+  let didCreateMtUser = false;
+  let encUser;
+  let vmtUser;
 
-  // Do not want to login a user who is being created by a currently logged in user
-  // Currently only comes from encompass
+  try {
+    let currentUser = getUser(req);
 
-  let doLoginNewUser = currentUser === undefined;
+    // Do not want to login a user who is being created by a currently logged in user
+    // Currently only comes from encompass
 
-  let mtUser = req.mt.auth.signup.user;
+    let doLoginNewUser = currentUser === undefined;
 
-  if (mtUser === undefined) {
-    // should never happen
-    return next();
-  }
-  let apiToken = await generateAPIToken(mtUser._id);
+    mtUser = req.mt.auth.signup.user;
 
-  let [encUser, vmtUser] = await Promise.all([
-    createEncUser(mtUser, req.body, apiToken, 'vmt'),
-    createVmtUser(mtUser, req.body, apiToken, 'vmt'),
-  ]);
+    if (mtUser === undefined) {
+      // should never happen
+      return next();
+    }
 
-  mtUser.encUserId = encUser._id;
-  mtUser.vmtUserId = vmtUser._id;
-
-  await mtUser.save();
-
-  let accessToken;
-  let refreshToken;
-
-  if (doLoginNewUser) {
-    [accessToken, refreshToken] = await Promise.all([
-      generateAccessToken(mtUser),
-      generateRefreshToken(mtUser),
+    [encUser, vmtUser] = await Promise.all([
+      createEncUser(mtUser, req.body, 'vmt'),
+      createVmtUser(mtUser, req.body, 'vmt'),
     ]);
 
-    setSsoCookie(res, accessToken);
-    setSsoRefreshCookie(res, refreshToken);
-  }
+    if (encUser === null || vmtUser === null) {
+      if (encUser !== null) {
+        // vmt errored but enc was successful
+        // revert creating of enc user
+        await EncUser.findByIdAndDelete(encUser._id);
+      } else if (vmtUser !== null) {
+        await VmtUser.findByIdAndDelete(vmtUser._id);
+      }
+      // both creation processes failed
+      // just return error
+      return next(
+        new createError[500](
+          'Sorry, an unexpected error occured. Please try again.',
+        ),
+      );
+    }
 
-  let userEmail = mtUser.email;
-  if (typeof userEmail === 'string') {
-    let token = await getResetToken(20);
-
-    mtUser.confirmEmailToken = token;
-    mtUser.confirmEmailExpires = Date.now() + CONFIRM_EMAIL_TOKEN_EXPIRY; // 1 day
+    mtUser.encUserId = encUser._id;
+    mtUser.vmtUserId = vmtUser._id;
 
     await mtUser.save();
-    sendEmailSMTP(
-      userEmail,
-      process.env.VMT_URL,
-      'confirmEmailAddress',
-      token,
-      mtUser,
-      AppNames.Vmt,
-    );
-    // in production send new user email to all admins
-    // otherwise send to test gmail account
 
-    if (process.env.NODE_ENV === 'production') {
-      sendEmailsToAdmins(
-        process.env.VMT_URL,
-        AppNames.Vmt,
-        'newUserNotification',
-        mtUser,
-      );
-    } else {
+    let accessToken;
+    let refreshToken;
+
+    if (doLoginNewUser) {
+      [accessToken, refreshToken] = await Promise.all([
+        generateAccessToken(mtUser),
+        generateRefreshToken(mtUser),
+      ]);
+
+      setSsoCookie(res, accessToken);
+      setSsoRefreshCookie(res, refreshToken);
+    }
+
+    let userEmail = mtUser.email;
+    if (typeof userEmail === 'string') {
+      let token = await getResetToken(20);
+
+      mtUser.confirmEmailToken = token;
+      mtUser.confirmEmailExpires = new Date(
+        Date.now() + CONFIRM_EMAIL_TOKEN_EXPIRY,
+      ); // 1 day
+
+      await mtUser.save();
       sendEmailSMTP(
-        process.env.VMT_GMAIL_USERNAME,
+        userEmail,
         process.env.VMT_URL,
-        'newUserNotification',
-        null,
+        'confirmEmailAddress',
+        token,
         mtUser,
         AppNames.Vmt,
+      );
+      // in production send new user email to all admins
+      // otherwise send to test gmail account
+
+      if (process.env.NODE_ENV === 'production') {
+        sendEmailsToAdmins(
+          process.env.VMT_URL,
+          AppNames.Vmt,
+          'newUserNotification',
+          mtUser,
+        );
+      } else {
+        sendEmailSMTP(
+          process.env.VMT_GMAIL_USERNAME,
+          process.env.VMT_URL,
+          'newUserNotification',
+          null,
+          mtUser,
+          AppNames.Vmt,
+        );
+      }
+    }
+
+    let results = {
+      accessToken,
+      refreshToken,
+      vmtUser,
+    };
+
+    res.json(results);
+  } catch (err) {
+    console.log({ vmtSignupError: err });
+    if (didCreateMtUser === false) {
+      // error creating sso user
+      // revert enc / vmt creation if necessary
+      if (encUser) {
+        EncUser.findByIdAndDelete(encUser._id);
+      }
+      if (vmtUser) {
+        VmtUser.findByIdAndDelete(vmtUser._id);
+      }
+      return next(
+        new createError[500](
+          'Sorry, an unexpected error occured. Please try again.',
+        ),
       );
     }
   }
-
-  let results = {
-    accessToken,
-    refreshToken,
-    vmtUser,
-  };
-
-  res.json(results);
 };
