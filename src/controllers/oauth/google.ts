f759c2f44209de6ce14c76f486ca82bf77e091ae
anzook <azook@21pstem.org>
2021-11-02 08:32:17 -0400

    Pending google user addl fix

--- src/controllers/oauth/google.ts
+++ src/controllers/oauth/google.ts
@@ -126,44 +126,47 @@ export const googleCallback = async (
 
       let isNewUser = isNil(mtUser.encUserId);
 
-      // catch if pending user data was not added to existing VMT user
-      // Matches for VMT user with same email and 'pending' accountType
-      // env toggle to enable feature
-      if (
-        process.env.VMT_YES_TO_CONVT_PENDING &&
-        process.env.VMT_YES_TO_CONVT_PENDING.toLowerCase() === 'yes'
-      ) {
-        // find VMT user with data
-        const vmtPendingData = await VmtUser.findOne({
-          email: mtUser.email,
-          accountType: 'pending',
-        });
-        // find duplicate VMT user to remove
-        const vmtMTdata = await VmtUser.findOne({
-          email: mtUser.email,
-          ssoId: mtUser._id,
-        });
+      if (!isNewUser) {
+        // catch if pending user data was not added to existing VMT user
+        // Matches for VMT user with same email and 'pending' accountType
+        // env toggle to enable feature
         if (
-          vmtPendingData &&
-          vmtPendingData.accountType === 'pending' &&
-          vmtMTdata
+          process.env.VMT_YES_TO_CONVT_PENDING &&
+          process.env.VMT_YES_TO_CONVT_PENDING.toLowerCase() === 'yes'
         ) {
-          // Swap linked Ids to link sso account with VMT account with data
-          await VmtUser.findByIdAndUpdate(vmtPendingData._id, {
+          // find VMT user with data
+          const vmtPendingData = await VmtUser.findOne({
+            email: mtUser.email,
+            accountType: 'pending',
+          });
+          // find duplicate VMT user to remove
+          const vmtMTdata = await VmtUser.findOne({
+            email: mtUser.email,
             ssoId: mtUser._id,
-            accountType: 'facilitator',
           });
-          mtUser.vmtUserId = vmtPendingData._id;
-          await mtUser.save();
+          if (
+            vmtPendingData &&
+            vmtPendingData.accountType === 'pending' &&
+            vmtMTdata
+          ) {
+            // Swap linked Ids to link sso account with VMT account with data
+            await VmtUser.findByIdAndUpdate(vmtPendingData._id, {
+              ssoId: mtUser._id,
+              accountType: 'facilitator',
+              isEmailConfirmed: true,
+            });
+            mtUser.vmtUserId = vmtPendingData._id;
+            await mtUser.save();
 
-          // invalidate or delete duplicate record
-          await VmtUser.findByIdAndUpdate(vmtMTdata._id, {
-            isTrashed: true,
-            accountType: 'temp',
-            email: '',
-          });
-          // await VmtUser.findByIdAndUpdate(vmtMTdata._id, { ssoId: '' });
-          // await VmtUser.findByIdAndDelete(vmtMTdata._id);
+            // invalidate or delete duplicate record
+            await VmtUser.findByIdAndUpdate(vmtMTdata._id, {
+              isTrashed: true,
+              accountType: 'temp',
+              email: '',
+            });
+            // await VmtUser.findByIdAndUpdate(vmtMTdata._id, { ssoId: '' });
+            // await VmtUser.findByIdAndDelete(vmtMTdata._id);
+          }
         }
       }
 
