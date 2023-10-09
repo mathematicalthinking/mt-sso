04025b0c1a5bc20050d84bedae847afda95b7fde
Yousof Wakili <ywakili18@gmail.com>
2023-10-09 14:36:19 -0700

    Added rout, update username logic in controllers and auth

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import createError from 'http-errors';
-
+import axios from 'axios';
 import { getUser } from '../middleware/user-auth';
 import User from '../models/User';
+import { ObjectId } from 'mongodb';
 
 export const put = async (
   req: express.Request,
@@ -41,3 +42,52 @@ export const put = async (
     next(err);
   }
 };
+// What is the URL for each app?
+const VMT_URL = process.env.VMT_URL;
+const ENC_URL = process.env.ENC_URL;
+
+const updateUserName = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction,
+): Promise<void> => {
+  try {
+    const userId = req.params.id;
+    const userName = req.body.newUserName;
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return next(createError(404, 'User does not exist'));
+    }
+
+    user.name = userName;
+    await user.save();
+
+    const vmtResponse = await axios.put(VMT_URL, {
+      userId,
+      username: userName,
+    });
+
+    if (vmtResponse.status !== 200 || !vmtResponse.data.isSuccess) {
+      throw new Error('Failed to update username in VMT');
+    }
+
+    const encResponse = await axios.put(ENC_URL, {
+      userId,
+      username: userName,
+    });
+
+    if (encResponse.status !== 200 || !encResponse.data.isSuccess) {
+      throw new Error('Failed to update username in Encompass');
+    }
+
+    res.json({
+      isSuccess: true,
+    });
+  } catch (err) {
+    console.error(err);
+    next(createError(500, 'Failed to update username'));
+  }
+};
+
+export { updateUserName };
--- src/routes/auth/user.ts
+++ src/routes/auth/user.ts
@@ -4,5 +4,6 @@ const router = Router();
 import * as controller from '../../controllers/user';
 
 router.put('/:id', controller.put);
+router.put('/:id/name', controller.updateUserName);
 
 export default router;
